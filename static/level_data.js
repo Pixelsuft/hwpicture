042f4bf078587742259ee33543bb88e6cf7ae2ca
pixelsuft‮ <68371847+Pixelsuft@users.noreply.github.com>
2021-03-18 20:43:03 +0700

    Add files via upload

--- static/level_data.js
+++ static/level_data.js
@@ -6,9 +6,27 @@ document.getElementById('go_home').addEventListener('click', function () {
     location.href = '/';
 });
 
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
 document.getElementById('hide_other').addEventListener('click', function () {
     document.getElementById('main_h1').style.display = 'none';
     document.getElementById('result_title').style.display = 'none';
     document.getElementById('go_home').style.display = 'none';
+    document.getElementById('download_it').style.display = 'none';
     document.getElementById('hide_other').style.display = 'none';
+});
+
+document.getElementById('download_it').addEventListener('click', function () {
+    download('data.txt', document.getElementById('result').innerText);
 });
